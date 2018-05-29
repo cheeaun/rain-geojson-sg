f716b6fd89350f2aff64da9e907884a51f84b96b
Lim Chee Aun <cheeaun@gmail.com>
2018-05-29 15:34:35 +0800

    Add fake `server.js` for running `npm start`

---
+++ server.js
@@ -0,0 +1,11 @@
+const spawn = require('child_process').spawn;
+const child = spawn('npm', ['start']);
+child.stdout.on('data', (data) => {
+  console.log(data.toString());
+});
+child.stderr.on('data', (data) => {
+  console.log(data.toString());
+});
+child.on('close', (code) => {
+  console.log('closing code: ' + code);
+});
