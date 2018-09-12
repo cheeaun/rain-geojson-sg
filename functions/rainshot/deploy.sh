12a3f2a6bf27048c479d83e5dbfa393110ed3a68
Lim Chee Aun <cheeaun@gmail.com>
2018-09-12 20:37:49 +0800

    Cloud function for taking screenshots

---
+++ functions/rainshot/deploy.sh
@@ -0,0 +1,1 @@
+gcloud beta functions deploy rainshot --trigger-http --runtime nodejs8 --memory 2048MB --region asia-northeast1
---
+++ functions/rainshot/index.js
@@ -0,0 +1,44 @@
+const puppeteer = require('puppeteer');
+let page;
+
+async function getBrowserPage() {
+  const browser = await puppeteer.launch({
+    defaultViewport: {
+      width: 640,
+      height: 361,
+      isLandscape: true,
+    },
+    args: [
+      '--disable-dev-shm-usage',
+      '--disable-setuid-sandbox',
+      '--no-first-run',
+      '--no-sandbox',
+      '--no-zygote',
+      '--single-process',
+    ],
+  });
+
+  const p = await browser.newPage();
+  await p.goto('https://checkweather.sg/#immersive', {
+    waitUntil: 'networkidle0',
+  });
+  await p.waitForFunction("$map.queryRenderedFeatures({layers: ['tempreadings']}).length > 0");
+  await p.waitForFunction("$map.queryRenderedFeatures({layers: ['humidreadings']}).length > 0");
+  await p.waitForFunction("$map.queryRenderedFeatures({layers: ['windirections']}).length > 0");
+
+  return Promise.resolve(p);
+}
+
+exports.rainshot = async (req, res) => {
+  if (!page) page = await getBrowserPage();
+
+  await page.waitForSelector('#datetime:not([hidden]) blink');
+  await page.waitForSelector('#loader[hidden]');
+
+  const imageBuffer = await page.screenshot({
+    type: 'jpeg',
+    quality: 90,
+  });
+  res.set('Content-Type', 'image/jpeg');
+  res.send(imageBuffer);
+};
