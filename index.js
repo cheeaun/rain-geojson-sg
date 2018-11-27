52cf317c19c8b5ec75c9e9865025f039825a4ec1
Lim Chee Aun <cheeaun@gmail.com>
2018-11-28 00:19:23 +0800

    15 minutes cache, a bit more aggressive

--- index.js
+++ index.js
@@ -390,7 +390,7 @@ module.exports = cors(async (req, res) => {
         });
       }
       res.setHeader('content-type', 'application/atom+xml');
-      res.setHeader('cache-control', 'public, max-age=600');
+      res.setHeader('cache-control', 'public, max-age=900, s-maxage=900');
       res.end(feed.atom1());
       break;
     default:
