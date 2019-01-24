b5f79221003debcdb5272edd121966ae74d2b547
Lim Chee Aun <cheeaun@gmail.com>
2019-01-24 14:49:49 +0800

    Add `--project` to deploy script to prevent going to wrong project ID

--- functions/rainshot/deploy.sh
+++ functions/rainshot/deploy.sh
@@ -1,1 +1,1 @@
-gcloud beta functions deploy rainshot --trigger-http --runtime nodejs8 --memory 2048MB --region asia-northeast1
+gcloud beta functions deploy rainshot --trigger-http --runtime nodejs8 --memory 2048MB --region asia-northeast1 --project rain-geojson-sg
