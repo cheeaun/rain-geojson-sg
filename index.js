1013d9d2a43c3a0ced7dd34ec7e6df33a94e10ff
Lim Chee Aun <cheeaun@gmail.com>
2020-01-05 23:28:11 +0800

    Fix stupid now-micro appending these lines of code

--- index.js
+++ index.js
@@ -419,20 +419,3 @@ module.exports = cors(async (req, res) => {
       res.end('404.');
   }
 });
-
-    let __original_lambda
-
-    if (typeof exports === 'function') {
-      __original_lambda = exports
-    }
-    else if (typeof module.exports === 'function') {
-      __original_lambda = module.exports
-    }
-    else {
-      throw new Error(
-        `now-micro builder expects main export to be a function (${typeof module.exports} found)`,
-      )
-    }
-
-    exports = module.exports = (req, res) => require('micro').run(req, res, __original_lambda)
-  
